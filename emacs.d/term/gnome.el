a7b017a90f808b1ce9faecb5a0d31ae08f69e40f
Kevin Pratt <kevin@ouelong.com>
2015-06-29 07:47:47 -0600

    clean out the emacs.d to start building it back again

--- emacs.d/term/gnome.el
+++
@@ -1,9 +0,0 @@
-(defvar foo-gnome "loaded gnome.el")
-
-(load "term/xterm")
-
-(defun terminal-init-gnome ()
-   "Terminal initialization function for gnome terminal."
-   ;; Use the xterm color initialization code.
-   (xterm-register-default-colors)
-   (tty-set-up-initial-frame-faces))
--- emacs.d/term/screen.el
+++
@@ -1,7 +0,0 @@
-(load "term/xterm")
-
-(defun terminal-init-screen ()
-   "Terminal initialization function for GNU screen."
-   ;; Use the xterm color initialization code.
-   (xterm-register-default-colors)
-   (tty-set-up-initial-frame-faces))

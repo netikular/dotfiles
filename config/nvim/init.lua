3a2cfd4139fb67ce1e1f37f565db8ed26b85c090
Kevin Pratt <kevin@paradem.co>
2024-02-05 13:19:20 -0700

    Update languages

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -334,7 +334,7 @@ vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers, { desc = 
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'lua', 'vim', 'ruby', 'heex', 'eex', 'elixir' },
+  ensure_installed = { 'lua', 'vim', 'ruby', 'heex', 'eex', 'elixir', 'bash' },
   --'rust', 'tsx', 'typescript', 'help', 'vim', 'elixir',
   --'eex', 'heex' },
 
